d2846a17c57d11698d8f3b270f2892db910180ee
pairofAces <karanchauhan16@gmail.com>
2020-12-20 16:57:20 -0500

    created component outline in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Making a LinkedIn Clone Part 1</h1>
+
+      {/* These are the components I will be building */}
+
+      {/* Header */}
+      {/* App Body */}
+      {/* Sidebar */}
+      {/* Feed */}
+      {/* Widgets */}
+
     </div>
   );
 }
