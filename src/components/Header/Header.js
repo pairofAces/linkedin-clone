6bb42dcc49b8ae607637eb262ceb9b4a7317a736
pairofAces <karanchauhan16@gmail.com>
2020-12-21 15:55:52 -0500

    added to the header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,9 @@ import SearchIcon from '@material-ui/icons/Search';
 import HeaderOption from './headerOption/HeaderOption';
 import HomeIcon from '@material-ui/icons/Home';
 import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import ChatIcon from '@material-ui/icons/Chat';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
 function Header() {
     return (
@@ -21,6 +24,9 @@ function Header() {
             <div className="header_right">
                 <HeaderOption Icon={HomeIcon } title="Home"/>
                 <HeaderOption Icon={SupervisorAccountIcon} title="My Network"/>
+                <HeaderOption Icon={BusinessCenterIcon} title="Jobs"/>
+                <HeaderOption Icon={ChatIcon} title="Messaging"/>
+                <HeaderOption Icon={NotificationsIcon} title="Notifications"/>
             </div>     
         </div>
     )
