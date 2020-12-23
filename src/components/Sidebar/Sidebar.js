e337fefc7c06c305227bbeb238cbf370e95aff8b
pairofAces <karanchauhan16@gmail.com>
2020-12-22 20:53:58 -0500

    working on sidebar css

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -11,8 +11,25 @@ function Sidebar() {
                 <h2>Karan S. Chauhan</h2>
                 <h4>karanchauhan16@gmail.com</h4>
             </div>
+
+            <div className="sidebar_stats">
+                {/* now I'll create individual stat divs for each stat
+                I want to display */}
+                <div className="sidebar_stat">
+                    <p>Who viewed you</p>
+                    <p className="sidebar_statNumber">1,738</p>
+                </div>
+                <div className="sidebar_stat">
+                    <p>Connections</p>
+                    <p className="sidebar_statNumber">9,000+</p>
+                </div>
+
+                <div className="sidebar_bottom">
+                    <p>Recent</p>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default Sidebar
+export default Sidebar;
