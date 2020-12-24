bc47d79fc9557fe348affb3d8ca2c4582d2568ac
pairofAces <karanchauhan16@gmail.com>
2020-12-23 22:41:11 -0500

    adding material UI icons

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './Feed.css';
+import CreateIcon from '@material-ui/icons/Create';
 
 function Feed() {
     return (
         <div className="feed">
-            
+            <div className="feed_inputContainer">
+                {/* this is where I will create the individual 
+                inputs within the top feed section */}
+                <div className="feed_input">
+                    <CreateIcon />
+                </div>
+            </div>
         </div>
     )
 }
