6f8b83388a7526c942568278f8b6da63acef19da
pairofAces <karanchauhan16@gmail.com>
2020-12-25 16:01:20 -0500

    4 post options are showing

--- src/components/Feed/Post.js
+++ src/components/Feed/Post.js
@@ -1,6 +1,11 @@
 import { Avatar } from '@material-ui/core';
 import React from 'react';
+import InputOption from './InputOption';
 import './Post.css';
+import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
+import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
+import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
+import SendOutlinedIcon from '@material-ui/icons/SendOutlined';
 
 function Post({ name, description, message, photoUrl }) {
     return (
@@ -8,13 +13,20 @@ function Post({ name, description, message, photoUrl }) {
             <div className="post_header">
                 <Avatar />
                 <div className="post_info">
-                    <h2>Karan S. Chauhan</h2>
-                    <p>Description</p>
+                    <h2>{name}</h2>
+                    <p>{description}</p>
                 </div>
             </div>
 
             <div className="post_body">
-                <p>Message goes here</p>
+                <p>{message}</p>
+            </div>
+
+            <div className="post_buttons">
+                <InputOption Icon={ThumbUpAltOutlinedIcon} title="Like" color="gray"/>
+                <InputOption Icon={ChatOutlinedIcon} title="Comment" color="gray"/>
+                <InputOption Icon={ShareOutlinedIcon} title="Share" color="gray"/>
+                <InputOption Icon={SendOutlinedIcon} title="Send" color="gray"/>
             </div>
         </div>
     )
