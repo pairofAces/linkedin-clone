b0356444f32a771f0ce2a021efd686b0cf445e92
pairofAces <karanchauhan16@gmail.com>
2020-12-26 21:30:46 -0500

    added firebase component and firebaseCongfig code

---
+++ src/components/Firebase/Firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDbDtxtz7W-iVkZN5Qq43xtxJbXZgOJQuA",
+  authDomain: "linkedin-clone-ead71.firebaseapp.com",
+  projectId: "linkedin-clone-ead71",
+  storageBucket: "linkedin-clone-ead71.appspot.com",
+  messagingSenderId: "657662109095",
+  appId: "1:657662109095:web:d69cdfaea23afc4dcf0282"
+};
