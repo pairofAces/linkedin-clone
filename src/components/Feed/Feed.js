d56bb34b7eeb3178aa4f4de33b74727d7a3c2702
pairofAces <karanchauhan16@gmail.com>
2020-12-27 19:50:24 -0500

    fixed post input bug

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -12,7 +12,7 @@ import firebase from 'firebase';
 
 
 function Feed() {
-    const [input, setInput] = useState('');
+    const [input, setInput] = useState("");
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
@@ -37,25 +37,21 @@ function Feed() {
             description: "This is a test, let's see if this works",
             message: input,
             photoUrl: '',
-            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
         })
     }
 
     return (
         <div className="feed">
             <div className="feed_inputContainer">
-                {/* this is where I will create the individual 
-                inputs within the top feed section */}
                 <div className="feed_input">
                     <CreateIcon />
                     <form>
-                        <input value={input} onChanage={(e) => setInput(e.target.value)} type="text"/>
+                        <input  value={ input } onChange={(e) => setInput(e.target.value)} type="text"/>
                         <button onClick={sendPost} type="submit">Send</button>
                     </form>
                 </div>
                 <div className="feed_inputOptions">
-                    {/* this will be where the input options are, for example,
-                    if I want to include a pic or a link, etc... */}
                     <InputOption Icon={ImageIcon} title="Photo" color="#7085F9"/>
                     <InputOption Icon={SubscriptionIcon} title="Video" color="#E7A33E"/>
                     <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD"/>
