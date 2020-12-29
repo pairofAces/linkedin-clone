291916881bb64f511e06ba8447422e4a846c34a2
pairofAces <karanchauhan16@gmail.com>
2020-12-28 23:19:59 -0500

    using a general linkedin logo - will change when login component is complete

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Login.css';
-import linkedUp from '../images/LinkedUp.png';
+// import linkedUp from '../images/LinkedUp.png';
 
 function Login() {
     return (
         <div className="login">
             {/* <h1>You're not logged in right now</h1> */}
-            <img src={linkedUp} alt=""/>
+            {/* <img src={linkedUp} alt=""/> */}
+            <img src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png?itok=q_1R0Vks" alt=""/>
 
             <form>
                 <input placeholder="Full Name {Required if registering}" type="text"/>
