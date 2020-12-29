320720375ddb96e48f9a731c829896f2d1918d82
pairofAces <karanchauhan16@gmail.com>
2020-12-28 23:01:45 -0500

    working on creating a login page

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import './App.css';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import Feed from './components/Feed/Feed';
+import { useSelector } from 'react-redux';
+import { selectUser } from './features/userSlice';
+import Login from './components/Login/Login';
 
 function App() {
+  const user = useSelector(selectUser);
+
+  
   return (
     <div className="app">
-
       <Header/>
-      <div className="app_body">
-        <Sidebar/>
-        <Feed />
-        {/* Widgets */}
-      </div>
+
+      {!user ? <Login /> : (
+    
+        <div className="app_body">
+          <Sidebar/>
+          <Feed />
+          {/* Widgets */}
+        </div>
+      )}
 
     </div>
   );
