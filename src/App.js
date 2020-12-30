bde8c4710b16dfe85b065d435d63ff5a86e02344
pairofAces <karanchauhan16@gmail.com>
2020-12-29 21:18:37 -0500

    working on useEffect in App.js -> line 14

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
@@ -6,10 +6,20 @@ import Feed from './components/Feed/Feed';
 import { useSelector } from 'react-redux';
 import { selectUser } from './features/userSlice';
 import Login from './components/Login/Login';
+import { auth } from './components/Firebase/Firebase';
 
 function App() {
   const user = useSelector(selectUser);
 
+  useEffect(() => {
+    auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        //the user is logged in
+      } else {
+        //the user is logged out
+      }
+    })
+  }, [])
   
   return (
     <div className="app">
