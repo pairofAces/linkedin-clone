bcc7e1480e963b7ce55340623559031d2c4b8906
pairofAces <karanchauhan16@gmail.com>
2020-12-29 21:03:43 -0500

    working on the register function

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { auth } from '../Firebase/Firebase.js';
+import { useDispatch } from 'react-redux';
+import { login } from '../../features/userSlice';
 // import linkedUp from '../images/LinkedUp.png';
 
 function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [name, setName] = useState("");
+    const [profilePic, setProfilePic] = useState("");
+    const dispatch = useDispatch();
 
     const register = () => {
+        if (!name) {
+            return alert("Please enter a full name");
+        }
 
-    }
+        auth.createUserWithEmailAndPassword(email, password)
+        .then((userAuth) => {
+            userAuth.user.updateProfile({
+                displayName: name,
+                photoURL: profilePic,
+            })
+            .then(() => {
+                dispatch(login({
+                    email: userAuth.user.email,
+                    uid: userAuth.user.uid,
+                    displayName: name,
+                    photoURL: profilePic
+                }));
+            });
+        });
+    };
 
     const signIn = (e) => {
         e.preventDefault();
@@ -24,13 +46,13 @@ function Login() {
             <img src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png?itok=q_1R0Vks" alt=""/>
 
             <form>
-                <input placeholder="Full Name {Required if registering}" type="text"/>
+                <input value={name} onChange={e => setName(e.target.value)} placeholder="Full Name {Required if registering}" type="text"/>
 
-                <input placeholder="Profile Picture URL (Optional)" type="text"/>
+                <input value={profilePic} onChange={e => setProfilePic(e.target.value)} placeholder="Profile Picture URL (Optional)" type="text"/>
 
-                <input placeholder="Email" type="email"/>
+                <input value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" type="email"/>
 
-                <input placeholder="Password" type="password"/>
+                <input value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" type="password"/>
 
                 <button type="submit" onClick={signIn}>Sign In</button>
             </form>
