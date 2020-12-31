8cd30da2f1cd136fea247cb59780022bc5130039
pairofAces <karanchauhan16@gmail.com>
2020-12-30 20:19:58 -0500

    user info is appearing on sidebar successfully

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,8 +1,13 @@
 import { Avatar } from '@material-ui/core';
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectUser } from '../../features/userSlice';
 import './Sidebar.css';
 
 function Sidebar() {
+    //import the user with redux
+    const user = useSelector(selectUser);
+
 
     //create a funciton 'recentItem' for the "Recent" section in sidebar
     const recentItem = (topic) => (
@@ -16,9 +21,9 @@ function Sidebar() {
         <div className="sidebar">
             <div className="sidebar_top">
                 <img src="https://images.unsplash.com/photo-1536599018102-9f803c140fc1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80" alt=""/>
-                <Avatar />
-                <h2>Karan S. Chauhan</h2>
-                <h4>karanchauhan16@gmail.com</h4>
+                <Avatar src={user.photoUrl} className="sidebar_avatar">{user.email[0]}</Avatar>
+                <h2>{user.displayName}</h2>
+                <h4>{user.email}</h4>
             </div>
 
             <div className="sidebar_stats">
